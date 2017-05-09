860a733ec53b7e1bf6114894f68890ea5f048e8e
Thomas Alunni <thomas.alunni-gradini@ynov.com>
2017-05-09 10:59:45 +0200

    Migration & Model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+	validates :name, presence: true
+	validates :text, presence: true
+
+	validates :photo, presence: true
+	has_attachment :photo, accept: [:jpg, :png, :jpeg]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :tweets
+  resources :tweets, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
+
+  #Index où se situeront les tweets et les photos
+  root to: "tweets#index"
 end
