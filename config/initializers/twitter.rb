ebbc80cd2a6a0e793dfc2eac2a30af671393c75b
Thomas Alunni <thomas.alunni-gradini@ynov.com>
2017-05-09 14:42:50 +0200

    API keys in twitter.rb (test keys, had to change it later)

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["QcobhjEdaEBuaMeCq9vT0Vyqz"]
+  config.consumer_secret     = ENV["s8FTYTVB4R9DAmppVMJjxz3tm6vcC3bup6JgspdoNyhuatOo8S"]
+  config.access_token        = ENV["854269952316121088-PDACooOEPdtWBgKD54cWnhzu7aXuX9j"]
+  config.access_token_secret = ENV["Cf0mKd1DGWeE8hJB4BMDU32VqzyuRaoxbdXn3Y9tVIbgK"]
+end
