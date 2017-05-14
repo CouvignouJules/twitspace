c275d7a152fe16bde3d194577a82e2a2b2d5e5b9
Cadrew <adrien.canino@ynov.com>
2017-05-14 16:36:32 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@
 
 Tweet.destroy_all
 
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "aiyzc2NDhAYsM32IWAJOjdFZy"
+  config.consumer_secret     = "CS6liN5XVFakOZupKlOsL23CuxSMlaH5KIhXig6P3MLTwY7kDj"
+  config.access_token        = "863747432793944065-Mh1k9o63U0qTpnh5ioYCq69hChAwpV5"
+  config.access_token_secret = "7TBfyPTftkP1okWxzFINGnCJ6Xit4jF82EK8FjHQ3skjO"
+end
+
+tweets = client.user_timeline('ISSCommunity', count: 20)
+
+tweet = Tweet.create!(text: tweets[0].text)
