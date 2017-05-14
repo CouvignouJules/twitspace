95d938a09b001d5a3d114159379b3ec2b2078411
Adrien Canino <adrien.canino@ynov.com>
2017-05-14 20:59:15 +0200

    Tweets and DB Improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,20 @@
 
 Tweet.destroy_all
 
-require 'twitter'
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "aiyzc2NDhAYsM32IWAJOjdFZy"
-  config.consumer_secret     = "CS6liN5XVFakOZupKlOsL23CuxSMlaH5KIhXig6P3MLTwY7kDj"
-  config.access_token        = "863747432793944065-Mh1k9o63U0qTpnh5ioYCq69hChAwpV5"
-  config.access_token_secret = "7TBfyPTftkP1okWxzFINGnCJ6Xit4jF82EK8FjHQ3skjO"
-end
-
-tweets = client.user_timeline('https://twitter.com/isscommunity', count: 100)
-
-tweets.each do |tweet|
-    Tweet.create!(text: tweet.full_text, 
-    			  photo: tweet.user.profile_image_url,
-    			  username: tweet.user.name,
-    			  media: defined?(tweet.media[0].media_url) ? tweet.media[0].media_url : "#") #TODO: Mettre un tableau dans la BDD et non pas que le 1er élément
-end
+#require 'twitter'
+#
+#client = Twitter::REST::Client.new do |config|
+#  config.consumer_key        = "aiyzc2NDhAYsM32IWAJOjdFZy"
+#  config.consumer_secret     = "CS6liN5XVFakOZupKlOsL23CuxSMlaH5KIhXig6P3MLTwY7kDj"
+#  config.access_token        = "863747432793944065-Mh1k9o63U0qTpnh5ioYCq69hChAwpV5"
+#  config.access_token_secret = "7TBfyPTftkP1okWxzFINGnCJ6Xit4jF82EK8FjHQ3skjO"
+#end
+#
+#tweets = client.user_timeline('https://twitter.com/isscommunity', count: 100)
+#
+#tweets.each do |tweet|
+#    Tweet.create!(text: tweet.full_text, 
+#    			  photo: tweet.user.profile_image_url,
+#    			  username: tweet.user.name,
+#    			  media: defined?(tweet.media[0].media_url) ? tweet.media[0].media_url : "#")
+#end
