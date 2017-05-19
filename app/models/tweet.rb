bf7369d49e36aa568ee05c46e4d2aeb51ef0259b
Thomas Alunni <thomas.alunni-gradini@ynov.com>
2017-05-19 16:27:21 +0200

    Added votes (like/unlike) functionnality (not implemented in shows)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,14 @@
 class Tweet < ApplicationRecord
-	has_many :comments 
+	has_many :comments
+	has_many :votes
+
+	def like(user)
+	  votes << Vote.new(user: user)
+	end
+
+	def unlike(user)
+	  votes.where(user_id: user.id).first.destroy
+	end
 
 	validates :text, presence: true
 	validates :username, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519115731) do
+ActiveRecord::Schema.define(version: 20170519135558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,16 +40,6 @@ ActiveRecord::Schema.define(version: 20170519115731) do
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "tweet_id"
-    t.integer  "likes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tweet_id"], name: "index_likes_on_tweet_id", using: :btree
-    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
-  end
-
   create_table "tweets", force: :cascade do |t|
     t.string   "text"
     t.string   "photo"
@@ -76,8 +66,19 @@ ActiveRecord::Schema.define(version: 20170519115731) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_votes_on_tweet_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
+  end
+
   add_foreign_key "comments", "tweets"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
+  add_foreign_key "votes", "tweets"
+  add_foreign_key "votes", "users"
 end
