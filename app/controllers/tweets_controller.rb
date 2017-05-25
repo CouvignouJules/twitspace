3942d87b246701b18c47bcb8a27bcf338a2cec9c
Adrien Canino <adrien.canino@ynov.com>
2017-05-25 19:53:33 +0200

    Popularity User Sotry

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,8 +4,10 @@ class TweetsController < ApplicationController
   	  	@new_comment = Comment.new
   	end
 	def index
- 	   	@tweets = Tweet.order(:id).page(params[:page]).per(10)
- 	   	#@tweets = Tweet.all
+		#Tweets : du plus récent au plus ancien
+ 	   	@tweets = Tweet.order("id").page(params[:page]).per(10)
+		#Tweets : du plus populaire au moins populaire
+ 	   	@tweets_pop = Tweet.order("likers_count DESC").page(params[:page_pop]).per(10)
  	end
  	def like
  		current_user.like!(Tweet.find(params[:id]))
