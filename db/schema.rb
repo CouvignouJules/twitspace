647ecc9a0894e6592f3249d66e86fdc77f5e39f8
Adrien Canino <adrien.canino@ynov.com>
2017-05-25 20:15:56 +0200

    Improvements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525143639) do
+ActiveRecord::Schema.define(version: 20170525093418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,9 +75,9 @@ ActiveRecord::Schema.define(version: 20170525143639) do
     t.string   "photo"
     t.string   "username"
     t.string   "media"
+    t.integer  "likers_count", default: 0
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.integer  "likers_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
@@ -99,17 +99,6 @@ ActiveRecord::Schema.define(version: 20170525143639) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "tweet_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tweet_id"], name: "index_votes_on_tweet_id", using: :btree
-    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
-  end
-
   add_foreign_key "comments", "tweets"
   add_foreign_key "comments", "users"
-  add_foreign_key "votes", "tweets"
-  add_foreign_key "votes", "users"
 end
